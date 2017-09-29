cb7896f2739076696087dfd7469d56e89414b8d3
Kristen Warnquist <kristen.warnquist@gmail.com>
2017-09-29 12:50:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,4 +4,15 @@ class TicTacToe
     @board = board || Array.new(9, " ")
   end
 
+  WIN_COMBINATIONS = [
+    [0,1,2], #top-row win
+    [3,4,5], #mid-row win
+    [6,7,8], #bottom-row win
+    [0,3,6], #left col win
+    [1,4,7], #mid col win
+    [2,5,8], #right col win
+    [0,4,8], #left diag win
+    [2,4,6]  #right diag win
+  ]
+
 end
